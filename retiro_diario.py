23d033fe2d45688d4c7d9db5a0f141735489d93b
Nelson-B26 <nelbalvin2005Qgmail.com>
2026-03-05 15:52:06 -0500

    Implementacion de segundo escenario; se ejecuta el retiro

--- retiro_diario.py
+++ retiro_diario.py
@@ -4,3 +4,7 @@ def control_de_retiro (monto, cuenta):
      if cuenta[ 'Retiro_diario'] + monto > cuenta['Limite_de_retiro']:
           print("Se ha superado el límite máximo de retiro permitido. su tope diario es de 500")
           return False
+     else:
+        cuenta[ 'Retiro_diario'] += monto
+        #retirar_dinero(monto, cuenta) #descomentar esta línea cuando la función retirar_dinero esté lista
+        return True
