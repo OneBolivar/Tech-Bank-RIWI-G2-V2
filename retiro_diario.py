dbe0c4a4166ccce0705515b742159f336828e4d5
Nelson-B26 <nelbalvin2005Qgmail.com>
2026-03-05 15:49:17 -0500

    Se define funcion del control de retiro- Configuracion inicial

---
+++ retiro_diario.py
@@ -0,0 +1,6 @@
+#importar la función retirar_dinero del módulo retiro cuando este lista
+
+def control_de_retiro (monto, cuenta):
+     if cuenta[ 'Retiro_diario'] + monto > cuenta['Limite_de_retiro']:
+          print("Se ha superado el límite máximo de retiro permitido. su tope diario es de 500")
+          return False
