c42223bf73c8bcae8bd7788d448870a24ddc08f8
Daniel Mendoza <danielmendoza1515@gmail.com>
2026-03-05 23:46:32 -0500

    escenario de prueba de las fuciones de guardar y mostrar historial

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("ESTE ES EL MAIN")
+from registro_operaciones import historial_operaciones, mostrar_historial
+
+historial_operaciones("Retirar", 200, 800)
+historial_operaciones("Consultar", 0, 800)
+historial_operaciones("Depositar", 500, 1300)
+historial_operaciones("Retirar", 100, 1200)
+
+mostrar_historial()
--- registro_operaciones.py
+++ registro_operaciones.py
@@ -8,9 +8,9 @@ def historial_operaciones (tipo, monto, saldo):
     })
 
 def mostrar_historial ():
-    print("=" * 50)
-    print("                   HISTORIAL DE OPERACIONES")
-    print("=" * 50)
+    print("=" * 30)
+    print("   HISTORIAL DE OPERACIONES")
+    print("=" * 30)
     for i , operacion in enumerate(historial,1):
         print(f" Operacion {i}---- {operacion['Tipo']}")
         print(f"  Monto:     ${operacion['Monto']:>10,.2f}")
