40812ba0b550728661320232743f2c2ef9dfa10c
Daniel Mendoza <danielmendoza1515@gmail.com>
2026-03-05 19:23:18 -0500

    Implementacion de funcion para mostrar  el historial de operaciones

--- registro_operaciones.py
+++ registro_operaciones.py
@@ -6,3 +6,12 @@ def historial_operaciones (tipo, monto, saldo):
         'Monto':monto,
         'Saldo':saldo
     })
+
+def mostrar_historial ():
+    print("=" * 50)
+    print("                   HISTORIAL DE OPERACIONES")
+    print("=" * 50)
+    for i , operacion in enumerate(historial,1):
+        print(f" Operacion {i}---- {operacion['Tipo']}")
+        print(f"  Monto:     ${operacion['Monto']:>10,.2f}")
+        print(f"  Saldo:     ${operacion['Saldo']:>10,.2f}")
