dfb985992442f0035d50cbe46ca56bc0677d5553
Lider <jebolivarp33@gmail.com>
2026-03-06 12:23:19 -0500

    Finalizado

--- deposito.py
+++ deposito.py
@@ -1,5 +1,7 @@
+from registro_operaciones import historial_operaciones
 def deposito(valor,cuenta):
   cuenta['saldo']+= valor
   print (" Su Deposito fue exitoso, su nuevo saldo es:", cuenta['saldo']) 
+  historial_operaciones("Depositar", valor, cuenta['saldo'])
 
 
